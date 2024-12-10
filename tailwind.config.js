46239c3a646672cdd6c39803315e1d65174dbdef
JasonTinotenda <kufakwatenziyason@gmail.com>
2024-12-11 01:44:54 +0200

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./trailblazers/index.{html,css,js}"],
+  content: ["./trailblazers/**/*.{html,css,js}"], // Scans all files with html, css, and js extensions in the trailblazers folder and its subfolders
   theme: {
-    extend: {},
+    extend: {}, // You can extend the default Tailwind theme here
   },
-  plugins: [],
-}
-
+  plugins: [], // Add plugins here if needed
+};
